e6bbeb5939b1824506c6a5c8dd25aa250681f6a2
Gustavo Keiji <gugu.keiji.2004@gmail.com>
2021-05-11 14:06:15 -0300

    Correção de bugs

--- Ex5/Program.cs
+++ Ex5/Program.cs
@@ -8,22 +8,22 @@ namespace Ex5
         {
             Console.WriteLine("Tabuada do 1 ao 10");
             int i2 = 1;
-            bool check = true;
-            for (var i = 1; check == true; i++)
+            // bool check = true;
+            for (var i = 1; i <= 11; i++)
             {
                 if (i == 11)
                 {
-                    if (i2 <= 9)
-                    {
+                    // if (i2 <= 9)
+                    // {
                         i2++;
                         i = 1;
-                    }
+                    // }
                 }
                 int resultado = i2 * i;
                 Console.WriteLine($"{i2} x {i} = {resultado}");
                 if (resultado == 100)
                 {
-                    check = false;
+                    break;
                 }
             }
         }
